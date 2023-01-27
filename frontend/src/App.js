805b6f273fc71988c5715b5232e1fc94ca7fb5c2
p-reetu <79264123+p-reetu@users.noreply.github.com>
2023-01-27 13:39:24 +0530

    new pages and routes added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,8 @@ import HomePage from './components/homepage/HomePage';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import SellerDashboard from './components/sellerdashboard/sellerdashboard';
+import ProductPage from './components/productpage/ProductPage';
+import SellerPage from './components/sellerpage/SellerPage';
 
 function App() {
   return (
@@ -13,6 +15,8 @@ function App() {
       <Routes>
         <Route path='/' element={<SignPage/>}/>
         <Route path='/home/:id' element={<HomePage/>}/>
+        <Route path='/product/:id' element={<ProductPage/>}/>
+        <Route path='/:name' element={<SellerPage/>}/>
         <Route path='/seller/:id' element={<SellerDashboard/>}/>
       </Routes>
       </BrowserRouter>
