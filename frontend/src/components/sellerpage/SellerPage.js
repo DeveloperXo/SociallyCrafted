debfed14842391892945639e5feee115293bec0d
p-reetu <79264123+p-reetu@users.noreply.github.com>
2023-03-17 23:40:37 +0530

    neww

--- frontend/src/components/sellerpage/SellerPage.js
+++ frontend/src/components/sellerpage/SellerPage.js
@@ -14,14 +14,14 @@ function SellerPage() {
                     <div className="left">
                         <h1>crafty_home_decor</h1>
                         <span>Crafty home decoration <br></br>Crafty home decoration things that enhance the beauty of your home.<br></br>Jaipur, Maharashtra.</span>
-                        <span className="blueSpan"><sapn className="num">59</sapn> <sapn className="text">Posts</sapn>   <span className="num">600</span><span className="text"> Followers</span></span>
+                        <span className="blueSpan"><sapn className="num">59</sapn> <sapn className="text">Products</sapn>   <span className="num">600</span><span className="text"> Followers</span></span>
                         <div className="rsbuttons">
                     <button className="bluebutton">Follow</button>
-                    <button className="whitebutton">Message</button>
+                    
                     </div>
                     </div>
                     <div className="right">
-                        <img alt="" src={pimage}></img>
+                        <img style={{height:"250px", width:"250px" }} alt="" src="https://i.fbcd.co/products/resized/resized-750-500/709af4d6e2926394e1808a2ca3ed35a28af1c37bec37e5183c922e0b26522a58.jpg"></img>
                     </div>
                 </div>
                 <div className="lowerSide">
